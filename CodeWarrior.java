472af54473b859b16963c1cbf21e7c284ebb8e97
Naomi Hsieh <nhsieh60@stuy.edu>
2025-01-09 11:01:49 -0500

    editing codewarrior

--- CodeWarrior.java
+++ CodeWarrior.java
@@ -1,56 +1,51 @@
 public class CodeWarrior extends Adventurer{
-  int caffeine, caffeineMax;
-  String preferredLanguage;
+  int pH, pHmax;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public CodeWarrior(String name, int hp, String language){
+  public CodeWarrior(String name, int hp){
     super(name,hp);
-    caffeineMax = 12;
-    caffeine = caffeineMax/2;
-    preferredLanguage = language;
+    pHMax = 14;
+    pH = pHMax/2;
   }
 
-  public CodeWarrior(String name, int hp){
-    this(name,hp,"c++");
-  }
 
   public CodeWarrior(String name){
-    this(name,24);
+    this(name,25);
   }
 
   public CodeWarrior(){
-    this("Carmack");
+    this("Chompie");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "caffeine";
+    return "pH";
   }
 
   public int getSpecial(){
-    return caffeine;
+    return pH;
   }
 
   public void setSpecial(int n){
-    caffeine = n;
+    pH = n;
   }
 
   public int getSpecialMax(){
-    return caffeineMax;
+    return pHMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 caffeine*/
+  /*Deal 2-7 damage to opponent, restores 2 pH*/
   public String attack(Adventurer other){
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
-    return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. They then take a sip of their coffee.";
+    return this + " used Fly Trap! They snatched "+ other + " in their jaws and dealt "+ damage +
+    " points of damage. They then absorbed those";
   }
 
-  /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-  *Reduces caffeine by 8.
+  /*Deal 3-12 damage to opponent, only if pH is high enough.
+  *Reduces pH by 8.
   */
   public String specialAttack(Adventurer other){
     if(getSpecial() >= 8){
@@ -61,7 +56,7 @@ public class CodeWarrior extends Adventurer{
       " skills to hack the matrix. "+
       " This glitched out "+other+" dealing "+ damage +" points of damage.";
     }else{
-      return "Not enough caffeine to use the ultimate code. Instead "+attack(other);
+      return "Not enough pH to use the ultimate code. Instead "+attack(other);
     }
 
   }
